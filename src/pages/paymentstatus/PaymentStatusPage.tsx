a3dbc612d31fb7c3e6fba82d8e78b34c8d8a754f
xuanhanhbn <xuanhanhbn2@gmail.com>
2023-09-10 10:17:03 +0700

    init commit

--- src/pages/paymentstatus/PaymentStatusPage.tsx
+++ src/pages/paymentstatus/PaymentStatusPage.tsx
@@ -10,7 +10,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary">
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" href="https://mui.com/vn">
         Your Website
       </Link>{" "}
       {new Date().getFullYear()}
